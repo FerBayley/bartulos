a8563442c9c2c839cf6d7b312c0af585adc00d4d
fernando.bayley@gmail.com <fernando.bayley@gmail.com>
2016-08-17 02:39:00 -0300

    Finalizacion de la home

---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="es-AR">
+<head>
+	<meta charset="UTF-8">
+	<title>Bartulos Bazar</title>
+	<link rel="stylesheet" href="public/css/normalize.css" />
+	<link rel="stylesheet" href="public/css/main.css" />
+	<link href="https://fonts.googleapis.com/css?family=Biryani" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+</head>
+<body>
+
+	<section class="Container">
+		
+		<header>
+			<section class="Menu">
+				<h1><img src="public/images/logo.png" alt="Bartulos Bazar"></h1>
+				<ul>
+					<li><span>HOME</span></li>
+					<li><a href="#">EMPRESA</a></li>
+					<li><a href="#">PRODUCTOS</a></li>
+					<li><a href="#">NOVEDADES</a></li>
+					<li><a href="#">CONTACTO</a></li>
+				</ul>
+
+				<div class="Redes">
+					<ul>
+						<li><a href="#">
+							<img src="public/images/facebook.png" alt="Facebook">
+						</a></li>
+						<li><a href="#">
+							<img src="public/images/Instagram.png" alt="Instagram">
+						</a></li>
+					</ul>
+				</div> <!-- /Redes -->
+			</section> <!--/ Menu -->
+
+			<section class="Slider">
+				<img src="public/images/slider/img-1.png" alt="Sarten modelo tal por cual">
+			</section> <!--/Slider -->
+		</header> <!--/ header -->
+
+
+		<section class="Destacados">
+			<div class="Titulo">
+				<div class="Titulo-col1"></div> <!-- / Titulo-col1 -->
+				<div class="Titulo-col2">
+					<h2>DESTACADOS Y NOVEDADES</h2>
+					<br>
+					<h3>Sobre nuevos productos, ofertas, novedades</h3>
+				</div> <!-- / Titulo-col2 -->
+				<div class="Titulo-col3"></div> <!-- / Titulo-col2 -->
+			</div> <!-- / Titulo -->
+			
+			<div class="Imagen-1">
+				<img src="public/images/destacados/destacado1.png" alt="Imagen destacada">
+			</div> <!--/ Imagen-1 -->
+			<div class="Imagen-2">
+				<img src="public/images/destacados/destacado2.png" alt="Imagen destacada">
+			</div> <!--/ Imagen-2 -->
+			<div class="Imagen-3">
+				<img src="public/images/destacados/destacado3.png" alt="Imagen destacada">
+			</div> <!--/ Imagen-3 -->
+			<div class="Imagen-4">
+				<img src="public/images/destacados/destacado4.png" alt="Imagen destacada">
+			</div> <!--/ Imagen-4 -->
+		</section> <!--/ Destacados -->
+
+		<div class="Separdor"></div> <!--/ Separdor -->
+
+		<section class="Servicios">
+			<div class="Servicios-logo">
+				<img src="public/images/salero-big.png" alt="Bartulos Bazar">
+			</div> <!--/ Servicios-logo -->
+
+			<div class="Servicios-descripcion">
+				<article>
+					<p>
+						Bártulos, Bazar Gastronómico, nace con el deseo de poder acompañar, guiar, asesorar a los clientes en todas las compras de productos para que puedan satisfacer sus necesidades de la manera más productiva y competitiva para su organización.
+					</p>
+						<a href="#">
+							CONOCÉ NUESTROS SERVICIOS
+						</a>
+				</article>
+				
+			</div> <!-- /Servicios-descripcion -->
+		</section> <!--/ Servicios -->
+	
+		<section class="Path">
+			<div class="Path-footer"></div> <!-- /Path-footer -->
+		</section> <!-- / Path-footer -->
+
+		<footer>
+			<div class="footer-col1">
+				<img src="public/images/logo-footer.png" alt="Bartulos Bazar">
+			</div> <!-- /footer-col1 -->
+
+			<div class="footer-col2">
+				<p>Av. Lorem Ipsum 2056 / Buenos Aires</p>
+				<p>CP. 1480 - Tel. +54 (11) 4444 9293 |  Tel. +54 (11) 4434 9393</p>
+				<p>www.bartulosbazar.com.ar | info@bartulosbazar.com.ar</p>
+				<br>
+				<p>&copy; Copyrigth Bartulos - Todos los derechos reservados</p>
+			</div> <!-- /footer-col1 -->
+
+			<div class="footer-col3">
+				<ul>
+					<li><a href="#">HOME</a></li>
+					<li><a href="#">EMPRESA</a></li>
+					<li><a href="#">PRODUCTOS</a></li>
+					<li><a href="#">NOVEDADES</a></li>
+					<li><a href="#">CONTACTO</a></li>
+				</ul>
+			</div> <!-- /footer-col1 -->
+
+			<div class="footer-col4">
+				<ul class="Face">
+					<li>SEGUINOS</li>
+					<br>
+					<br>
+					<li><img src="public/images/facebook-footer.png" alt="Facebook Bartulos Bazar"></li>
+					<li><a href="#">bartulosbazar</a></li>
+				</ul>
+				<ul class="Insta">
+					<li><img src="public/images/instagram-footer.png" alt="Instagram Bartulos Bazar"></li>
+					<li><a href="#">@bartulosbazar</a></li>
+				</ul>
+			</div> <!-- /footer-col1 -->
+		</footer> <!-- /footer -->
+
+
+	</section> <!--/Container -->
+	
+</body>
+</html>
